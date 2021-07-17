633c5bd3f4a566829107f421c950a6a14396c97e
zaini <alimzaini@outlook.com>
2021-07-17 21:49:16 +0100

    Add highlighting to editors

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,9 @@ import { useState } from "react"
 import CodeEditor from "./components/CodeEditor"
 import { Message, Blocks, Elements } from 'slack-block-builder';
 
-const variables = `
-let channel = "channelname";
-let dangerLevel = 50;
-`
-
 const code = `
 Message()
-    .channel(channel)
+    .channel("Channel name")
     .text('Alas, my friend.')
     .blocks(
       Blocks.Section()
@@ -22,22 +17,32 @@ Message()
           Elements.Button()
             .text('Sure One Does')
             .actionId('gotClicked')
-            .danger(dangerLevel > 42), // Optional argument, defaults to 'true'
+            .danger(),
           Elements.Button()
             .text('One Does Not')
             .actionId('scaredyCat')
             .primary()))
 `
+const defaultPreviewLink = "https://api.slack.com/tools/block-kit-builder?blocks=%5B%7B%22text%22%3A%7B%22type%22%3A%22mrkdwn%22%2C%22text%22%3A%22One+does+not+simply+walk+into+Slack+and+click+a+button.%22%7D%2C%22type%22%3A%22section%22%7D%2C%7B%22text%22%3A%7B%22type%22%3A%22mrkdwn%22%2C%22text%22%3A%22At+least+that%27s+what+my+friend+Slackomir+said+%3Acrossed_swords%3A%22%7D%2C%22type%22%3A%22section%22%7D%2C%7B%22type%22%3A%22divider%22%7D%2C%7B%22elements%22%3A%5B%7B%22text%22%3A%7B%22type%22%3A%22plain_text%22%2C%22text%22%3A%22Sure+One+Does%22%7D%2C%22action_id%22%3A%22gotClicked%22%2C%22style%22%3A%22danger%22%2C%22type%22%3A%22button%22%7D%2C%7B%22text%22%3A%7B%22type%22%3A%22plain_text%22%2C%22text%22%3A%22One+Does+Not%22%7D%2C%22action_id%22%3A%22scaredyCat%22%2C%22style%22%3A%22primary%22%2C%22type%22%3A%22button%22%7D%5D%2C%22type%22%3A%22actions%22%7D%5D&mode=message"
+const defaultResult = `{"channel":"Channel name","text":"Alas, my friend.","blocks":[{"text":{"type":"mrkdwn","text":"One does not simply walk into Slack and click a button."},"type":"section"},{"text":{"type":"mrkdwn","text":"At least that's what my friend Slackomir said :crossed_swords:"},"type":"section"},{"type":"divider"},{"elements":[{"text":{"type":"plain_text","text":"Sure One Does"},"action_id":"gotClicked","style":"danger","type":"button"},{"text":{"type":"plain_text","text":"One Does Not"},"action_id":"scaredyCat","style":"primary","type":"button"}],"type":"actions"}]}`
 
 const App = () => {
   const [value, setValue] = useState(code)
-  const [result, setResult] = useState("")
+  const [result, setResult] = useState(defaultResult)
+  const [previewLink, setPreviewLink] = useState(defaultPreviewLink)
   const [error, setError] = useState("")
 
   const evaluateCode = () => {
     try {
-      const x = eval(`(function a(Message, Blocks, Elements) { ${variables} const res = ${value}.buildToJSON(); return res; })`)
+      const x = eval(`(function a(Message, Blocks, Elements) { const res = ${value}.buildToJSON(); return res; })`)
       const res = x(Message, Blocks, Elements)
+      const blocks = JSON.parse(res)["blocks"]
+
+      const q = new URLSearchParams()
+      q.append('blocks', JSON.stringify(blocks))
+      q.append('mode', 'message')
+      setPreviewLink(`https://api.slack.com/tools/block-kit-builder?${q}`)
+
       setResult(res)
     } catch (error) {
       setResult("")
@@ -48,7 +53,7 @@ const App = () => {
   return (
     <div>
       <h1>slack-block-builder-viewer</h1>
-      <CodeEditor value={value} setValue={setValue} />
+      <CodeEditor language="js" value={value} setValue={setValue} />
 
       <br />
 
@@ -57,7 +62,12 @@ const App = () => {
       <br />
 
       <h2>result</h2>
-      {result}
+
+      <a href={previewLink}>view on slack block builder</a>
+
+      <br />
+
+      <CodeEditor language="json" value={result} />
 
       <br />
 
--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,13 +1,21 @@
 import Editor from 'react-simple-code-editor';
-import { highlight, languages } from 'prismjs';
+import { Grammar, highlight, languages } from 'prismjs';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-json';
+import 'prismjs/themes/prism.css'
 
-const CodeEditor = ({ value, setValue }: { value: string, setValue: React.Dispatch<React.SetStateAction<string>> }) => {
+const languageMap: { [id: string]: Grammar } = {
+    "js": languages.js,
+    "json": languages.json
+}
+
+const CodeEditor = ({ language, value, setValue }: { language: string, value: string, setValue?: React.Dispatch<React.SetStateAction<string>> }) => {
     return (
         <div>
             <Editor
                 value={value}
-                onValueChange={(code: string) => setValue(code)}
-                highlight={(code: string) => highlight(code, languages.js, 'js')}
+                onValueChange={(code: string) => setValue && setValue(code)}
+                highlight={(code: string) => highlight(code, languageMap[language], language)}
                 padding={10}
                 style={{
                     fontFamily: '"Fira code", "Fira Mono", monospace',
