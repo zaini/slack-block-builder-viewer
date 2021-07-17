4c58083182657b5eca7daae249074eb0fc510081
zaini <alimzaini@outlook.com>
2021-07-17 21:25:48 +0100

    Add error message box and variables

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,46 @@ import { useState } from "react"
 import CodeEditor from "./components/CodeEditor"
 import { Message, Blocks, Elements } from 'slack-block-builder';
 
-const code = `console.log("howdy")`
+const variables = `
+let channel = "channelname";
+let dangerLevel = 50;
+`
+
+const code = `
+Message()
+    .channel(channel)
+    .text('Alas, my friend.')
+    .blocks(
+      Blocks.Section()
+        .text('One does not simply walk into Slack and click a button.'),
+      Blocks.Section()
+        .text('At least that\\'s what my friend Slackomir said :crossed_swords:'),
+      Blocks.Divider(),
+      Blocks.Actions()
+        .elements(
+          Elements.Button()
+            .text('Sure One Does')
+            .actionId('gotClicked')
+            .danger(dangerLevel > 42), // Optional argument, defaults to 'true'
+          Elements.Button()
+            .text('One Does Not')
+            .actionId('scaredyCat')
+            .primary()))
+`
 
 const App = () => {
   const [value, setValue] = useState(code)
   const [result, setResult] = useState("")
+  const [error, setError] = useState("")
 
   const evaluateCode = () => {
     try {
-      let x = eval(`(function a(Message, Blocks, Elements) { return ${value}.buildToJSON() })`)
-      setResult(x(Message, Blocks, Elements))
+      const x = eval(`(function a(Message, Blocks, Elements) { ${variables} const res = ${value}.buildToJSON(); return res; })`)
+      const res = x(Message, Blocks, Elements)
+      setResult(res)
     } catch (error) {
-      setResult(`${error}`)
+      setResult("")
+      setError(`${error}`)
     }
   }
 
@@ -30,6 +58,11 @@ const App = () => {
 
       <h2>result</h2>
       {result}
+
+      <br />
+
+      {error && [<h2>error</h2>, error]}
+
     </div>
   )
 }
